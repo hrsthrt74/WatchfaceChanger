332eb0258ad7c2b3f3f8c512f03d1a53bfc544f5
hrsthrt74 <49299205+hrsthrt74@users.noreply.github.com>
2023-06-22 18:40:25 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yifeplayte.watchfacechanger"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
